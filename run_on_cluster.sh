2125b34e290cee529513a346a4d09846563df822
zohair-aashiq <42138159+zohair-aashiq@users.noreply.github.com>
2019-05-11 10:22:14 +0200

    Update run_on_cluster.sh

--- run_on_cluster.sh
+++ run_on_cluster.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ./compile.sh;
-/home/oguz/Dev/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --class Main --master spark://localhost:7077 Overflow-processor-assembly-0.1.jar --files app.conf
+/home/zohair/Dev/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --class Main --master spark://localhost:7077 Overflow-processor-assembly-0.1.jar --files app.conf
